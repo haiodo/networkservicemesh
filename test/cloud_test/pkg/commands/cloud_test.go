7c513c172e2dc6f22c892c91887af2d486ccbf91
Andrey Sobolev <haiodo@xored.com>
2019-05-23 20:11:12 +0700

    Fix argumenst

--- test/cloud_test/pkg/commands/cloud_test.go
+++ test/cloud_test/pkg/commands/cloud_test.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"fmt"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"os"
 )
@@ -12,7 +13,7 @@ var rootCmd = &cobra.Command{
 	Long:  `Allow to execute all set of individual tests across all clouds provided.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// Do Stuff Here
-		fmt.Printf("Some stuff")
+		findTests(args)
 	},
 	Args: func(cmd *cobra.Command, args []string) error {
 		if len(args) < 1 {
@@ -37,7 +38,9 @@ func Execute() {
 
 type TestArguments struct {
 	providerConfig string // A folder to start scaning for tests inside.
-	junitXml string
+	junitXml string	// File to publish JUnti xml like report into
+	clusterTags string		// A list of tags to pass to do integration testing with one cluster.
+	multiTags string 	// tags for tests with multi cluster values.
 }
 
 var testArguments *TestArguments = &TestArguments{}
@@ -45,8 +48,16 @@ var testArguments *TestArguments = &TestArguments{}
 func init() {
 	cobra.OnInitialize(initConfig)
 	rootCmd.Flags().StringVarP(&testArguments.providerConfig, "providers", "p", "", "Config file for providers")
-	rootCmd.MarkFlagRequired("providers")
+	rootCmd.Flags().StringVarP(&testArguments.clusterTags, "tags", "t", "", "Tags list for cluster tests")
+	rootCmd.Flags().StringVarP(&testArguments.junitXml, "junitFile", "t", "", "Path to junit xml file")
+	rootCmd.Flags().StringVarP(&testArguments.multiTags, "multi", "m", "", "Tag list for multi cluster tests")
+	err := rootCmd.MarkFlagRequired("providers")
+	if err != nil {
+		logrus.Errorf("Error with command %v", err)
+	}
 }
 
 func initConfig() {
 }
+
+
---
+++ test/cloud_test/pkg/commands/process.go
@@ -0,0 +1,61 @@
+package commands
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"io"
+	"os/exec"
+)
+
+type proc struct {
+	cmd    *exec.Cmd
+	cancel context.CancelFunc
+	stdout io.ReadCloser
+	stderr io.ReadCloser
+}
+
+func execRead( ctx context.Context, args []string) (string, error) {
+	proc, error := execProc(ctx, args)
+	if error != nil {
+		return "", error
+	}
+	output := ""
+	reader := bufio.NewReader(proc.stdout)
+	for {
+		s, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		output += s + "\n"
+	}
+	return output, nil
+}
+
+func execProc(ctx context.Context, args []string) (proc, error) {
+	if len(args) == 0 {
+		return proc{}, fmt.Errorf("missing command to run")
+	}
+
+	ctx, cancel := context.WithCancel(ctx)
+	p := proc{
+		cmd:    exec.CommandContext(ctx, args[0], args[1:]...),
+		cancel: cancel,
+	}
+	logrus.Debugf("exec: %s", p.cmd.Args)
+	var err error
+	p.stdout, err = p.cmd.StdoutPipe()
+	if err != nil {
+		return p, err
+	}
+	p.stderr, err = p.cmd.StderrPipe()
+	if err != nil {
+		return p, err
+	}
+	err = p.cmd.Start()
+	if err == nil {
+		logrus.Debugf("go test pid: %d", p.cmd.Process.Pid)
+	}
+	return p, err
+}
